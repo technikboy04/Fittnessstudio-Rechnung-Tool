f08b8f09fc4f5b6d91a4430f5afc925d8c8042b8
hhoec <h.hoeckrich@gmx.de>
2021-04-30 11:42:13 +0200

    testuzzgz

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
@@ -12,7 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			//Test2
-			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			HBox root = (HBox)FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
